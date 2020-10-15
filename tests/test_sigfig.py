476a203e40a2bd1e700020239c452961f9c7d295
Monica Shapiro <monshapiro@gmail.com>
2020-10-15 10:41:22 -0400

    Added tests for sigfig function

--- tests/test_sigfig.py
+++ tests/test_sigfig.py
@@ -1,2 +1,10 @@
+from math import pi
+
+from plepy.helper import sigfig
+
 def test_sigfig__3sigfigs_of_pi_314():
-    pass
+    assert sigfig(pi, 3) == 3.14
+
+
+def test_sigfig__3sigfigs_of_neg_pi_neg314():
+    assert sigfig(-pi, 3) == -3.14
