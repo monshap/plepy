9ab9763a546d86b9fbee37716c004c09d589bcc7
Monica Shapiro <monshapiro@gmail.com>
2022-06-17 14:35:49 -0400

    updated requirements

--- setup.py
+++ setup.py
@@ -8,12 +8,12 @@ setup(
    author_email='monshapiro@gmail.com',
    packages=['plepy'],  #same as name
    install_requires=[
-       'numpy ~= 1.19',
-       'pyomo ~= 5.7',
-       'scipy ~= 1.5',
-       'matplotlib ~= 3.3',
-       'seaborn ~= 0.10',
-       'pandas ~= 1.1'
+       'numpy >= 1.19',
+       'pyomo >= 5.7',
+       'scipy >= 1.5',
+       'matplotlib >= 3.3',
+       'seaborn >= 0.10',
+       'pandas >= 1.1'
    ], #external packages as dependencies
    tests_require=['pytest']
 )
